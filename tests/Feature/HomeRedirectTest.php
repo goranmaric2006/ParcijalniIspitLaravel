1d1ab2351d399f4e7b1f040395caeefe9fbdb988
Goran Maric <goranmaric2006@gmail.com>
2024-12-19 19:07:43 +0100

    Simple test #4

---
+++ tests/Feature/HomeRedirectTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomeRedirectTest extends TestCase
+{
+    public function test_home_redirects_to_todos_page()
+    {
+        // Send a GET request to the root URL and assert it redirects to the 'todos.index' route
+        $response = $this->get('/');
+
+        $response->assertStatus(302); // 302 is the status code for redirects
+        $response->assertRedirect(route('todos.index'));
+    }
+}
