e701a317b06af62a33cc30185bf4ce996f7834a7
Goran Maric <goranmaric2006@gmail.com>
2024-12-19 18:44:53 +0100

    Add authentication to TodoTest #4

--- tests/Unit/TodoTest.php
+++ tests/Unit/TodoTest.php
@@ -11,31 +11,33 @@ class TodoTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Test that a todo item can be created.
+     *
+     * @return void
+     */
     public function test_todo_can_be_created()
     {
-        
+        // Create the user for authentication
         $user = User::create([
-            'name' => 'Test User',
+            'name' => 'Goran Marić',
             'email' => 'test@test.com',
             'password' => Hash::make('testtest'), 
         ]);
 
+        // Authenticate the user
+        $this->actingAs($user);
 
-        $response = $this->postJson('/login', [
-            'email' => 'test@test.com',
-            'password' => 'testtest',
+        // Send the POST request to create a new todo item
+        $response = $this->post('/todos', [
+            'title' => 'New Todo',
+            'description' => 'This is a new todo item',
         ]);
 
-        $response = $this->actingAs($user)
-                         ->postJson('/api/todos', [
-                             'title' => 'New Todo',
-                             'description' => 'This is a new todo item',
-                         ]);
-
-
+        // Assert the response status is 201 (Created)
         $response->assertStatus(201);
 
-
+        // Verify the todo is in the database
         $this->assertDatabaseHas('todos', [
             'title' => 'New Todo',
             'description' => 'This is a new todo item',
